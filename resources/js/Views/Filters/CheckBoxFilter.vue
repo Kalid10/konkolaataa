cb0d43c915d2b493db37b12eb65109a072ea3d46
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-29 20:01:05 +0300

    Add select all on checkbox filters

--- resources/js/Views/Filters/CheckBoxFilter.vue
+++ resources/js/Views/Filters/CheckBoxFilter.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {Checkbox} from "@/Components/shadcn/ui/checkbox/index.js";
 import Header from "@/Components/Header.vue";
+import {Switch} from "@/Components/shadcn/ui/switch/index.js";
 
 defineProps({
     items: {
@@ -14,22 +15,30 @@ defineProps({
     icon: {
         type: Object,
         required: true
+    },
+    showColorBadge:{
+        type: Boolean,
+        default: false,
     }
 })
 </script>
 
 <template>
-    <div class="flex flex-col space-y-2 text-sm py-4">
-        <div class="flex w-full items-center space-x-1">
+    <div class="flex flex-col space-y-2 text-sm">
+        <div class="flex w-full items-center space-x-1 pt-3">
             <component :is="icon" size="17"/>
             <div class="font-medium text-base">{{title}}</div>
         </div>
-        <div class="flex flex-col space-y-3 pl-1">
-
-            <div v-for="item in items" class="flex items-center space-x-2 w-full">
+        <div class="flex flex-col space-y-3">
+            <div class="flex items-center justify-between p-2 rounded-lg bg-slate-100">
+                <div>All {{title}}'s</div>
+                <Switch/>
+            </div>
+            <div v-for="item in items" class="flex items-center space-x-2 w-full pl-1">
                 <div class="flex space-x-2 items-center w-9/12">
                     <Checkbox class="w-3.5 h-3.5" />
                     <div class="flex items-center space-x-1">
+                        <div v-if="showColorBadge"  class="min-w-4 h-4 rounded-full border border-gray-300" :style="`background-color:` + item.value"></div>
                         <span>{{ item.name }}</span>
                     </div>
                 </div>
