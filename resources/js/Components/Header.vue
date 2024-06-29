fa0a93db83b8fd6ad6671685dc62345119e93620
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-30 00:35:02 +0300

    Update header component

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -13,7 +13,7 @@ defineProps({
 </script>
 
 <template>
-    <label class="flex flex-col ">
+    <label class="flex flex-col capitalize">
         <span class="font-semibold text-2xl md:text-4xl" v-if="title">{{ title }}</span>
         <span v-if="subtitle" class="block text-sm md:text-lg text-gray-700">{{ subtitle }}</span>
         <span v-else><slot /></span>
