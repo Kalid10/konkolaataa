72a72a15d831647faf4ea02683fd28f2d59c5414
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-29 12:45:09 +0300

    Add tag component

---
+++ resources/js/Components/Tag.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineEmits(['click'])
+defineProps({
+    value: {
+        type: String,
+        required: true
+    },
+})
+</script>
+
+<template>
+<div class="min-w-fit w-fit h-fit px-4 font-semibold cursor-pointer hover:scale-105 py-1 bg-gray-100 text-sm uppercase rounded-xl" @click="$emit('click')">
+    {{value}}
+</div>
+</template>
+
+<style scoped>
+
+</style>
