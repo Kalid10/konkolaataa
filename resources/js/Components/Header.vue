2677d6ae0c0872efb8c91b83e12ae62a65195a36
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-29 11:49:29 +0300

    Add `header` component

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: false
+    },
+    subtitle: {
+        type: String,
+        required: false
+    },
+
+});
+</script>
+
+<template>
+    <label class="flex flex-col ">
+        <span class="font-semibold text-2xl md:text-4xl" v-if="title">{{ title }}</span>
+        <span v-if="subtitle" class="block text-sm md:text-lg text-gray-700">{{ subtitle }}</span>
+        <span v-else><slot /></span>
+    </label>
+</template>
