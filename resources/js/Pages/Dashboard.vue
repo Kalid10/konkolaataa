d3302b01202f340893aeea55ae57260a2415df3c
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-06-29 11:53:37 +0300

    WIP

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,9 +5,7 @@ import { Head } from '@inertiajs/vue3';
 
 <template>
     <Head title="Dashboard" />
-
-    <AuthenticatedLayout>
-        <template #header>
+     <template>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
@@ -18,5 +16,4 @@ import { Head } from '@inertiajs/vue3';
                 </div>
             </div>
         </div>
-    </AuthenticatedLayout>
 </template>
