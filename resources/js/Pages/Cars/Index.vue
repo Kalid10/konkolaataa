e9a9734ffa9399fffd29c5b4eb95a1dac2780738
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-01 22:45:02 +0300

    Update theme and layout

--- resources/js/Pages/Cars/Index.vue
+++ resources/js/Pages/Cars/Index.vue
@@ -2,26 +2,26 @@
 
 import Header from "@/Components/Header.vue";
 import Items from "@/Views/Cars/Item.vue";
-import ConditionTab from "@/Views/Filters/DesktopFilters.vue";
+import DesktopFilter from "@/Views/Filters/DesktopFilters.vue";
 import UsedCarFilters from "@/Views/Cars/UsedCarFilters.vue";
 import NewCarFilters from "@/Views/Cars/NewCarFilters.vue";
 import FilterDrawer from "@/Views/Filters/FilterDrawer.vue";
 </script>
 
 <template>
-    <div class="flex flex-col space-y-6 md:space-y-8">
+    <div class="flex flex-col space-y-6 md:space-y-3">
         <Header title="Cars" subtitle="List of Cars" />
         <FilterDrawer/>
         <div class="w-full flex  justify-center md:space-x-12 md:justify-between">
             <div class="w-3/12 h-fit hidden md:flex flex-col items-center px-5 py-3 border border-gray-300 rounded-lg">
-                <ConditionTab>
+                <DesktopFilter>
                     <template #used_tab>
                        <UsedCarFilters/>
                     </template>
                     <template #new_tab>
                         <NewCarFilters/>
                     </template>
-                </ConditionTab>
+                </DesktopFilter>
             </div>
             <div class="w-full items-center md:items-start md:w-9/12 flex flex-col md:flex-row space-y-4 md:space-y-0 md:flex-wrap md:justify-between">
                 <Items v-for="i in 21" :seller-type="i % 2 === 0 ? 'Private Seller':'Broker'" />
