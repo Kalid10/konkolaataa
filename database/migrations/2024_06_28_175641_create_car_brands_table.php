f32c7e070bdcea38fdd84a029a752d3cdafd23fd
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-02 22:33:05 +0300

    Update car and car_brands table

--- database/migrations/2024_06_28_175641_create_car_brands_table.php
+++ database/migrations/2024_06_28_175641_create_car_brands_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('car_brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(Image::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Image::class)->nullable()->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
