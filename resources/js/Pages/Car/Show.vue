17a6c00e6b5a626f07ac9414c25d5a193510d533
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-04 15:52:16 +0300

    Fetch data from backend for show car component

--- resources/js/Pages/Car/Show.vue
+++ resources/js/Pages/Car/Show.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { Antenna, Fuel, MapPin,Facebook, Instagram, MessageSquareText, Send, Cog, Gauge, Car, Paperclip} from "lucide-vue-next";
-import InputLabel from "@/Components/InputLabel.vue";
+import { Antenna, Fuel, Cog, Gauge, Car, RectangleHorizontal, MapPin, ShieldBan} from "lucide-vue-next";
 import SellerInfo from "@/Views/Car/Show/SellerInfo.vue";
 import ShareItem from "@/Views/Car/Show/ShareItem.vue";
+
+defineProps({
+    car: {
+        type: Object,
+        required: true
+    },
+});
 </script>
 
 <template>
@@ -26,54 +32,69 @@ import ShareItem from "@/Views/Car/Show/ShareItem.vue";
         <div class="flex flex-col md:flex-row w-full justify-between">
         <div class="flex flex-col space-y-4 md:space-y-6 w-full md:bg-gray-50 md:px-5 md:pb-10 pt-5 rounded-lg md:w-6/12">
             <div class="flex justify-between items-center">
-                <div class="text-3xl md:text-4xl font-semibold">Volkswagen Polo 2002</div>
+                <div class="text-3xl md:text-4xl font-semibold">{{car.car_model.car_brand.name}} {{car.car_model.name}} </div>
             </div>
             <div class="flex justify-between md:items-end md:space-x-6 items-center">
-                <div class="text-xl md:text-2xl font-light border border-gray-600 px-2.5 py-0.5 rounded-lg w-fit md:bg-brand-primary md:text-white md:font-medium">940,000 Br</div>
-                <div class="text-sm md:font-medium md:text-base py-1 ">Slightly Negotiable</div>
+                <div class="text-xl md:text-2xl font-light border border-gray-600 px-2.5 py-0.5 rounded-lg w-fit md:bg-brand-primary md:text-white md:font-medium">{{car.price}} Br</div>
+                <div class="text-sm md:font-medium md:text-base py-1 uppercase">{{car.price_type}}</div>
             </div>
 
-            <SellerInfo class="md:hidden"/>
+            <SellerInfo :car="car" class="md:hidden"/>
 
             <div class="pt-3 font-semibold text-xl md:text-3xl">Overview</div>
 
             <div class="flex flex-col space-y-6 md:space-y-0 md:flex-row w-full md:justify-between py-2 md:text-lg">
                 <div class="flex flex-col space-y-6 md:w-5/12 md:justify-evenly">
-                    <div class="flex space-x-3 items-center font-medium">
+                    <div class="flex space-x-3 items-center font-medium uppercase">
                         <Antenna class="text-brand-primary"  size="15"/>
-                        <div>Used In Ethiopia</div>
+                        <div>{{car.car_condition_type.name}}</div>
                     </div>
-                    <div class="flex space-x-3 items-center font-medium">
+                    <div class="flex space-x-3 items-center font-medium uppercase">
                         <Antenna class="text-brand-primary"  size="15"/>
-                        <div>Automatic Transmission</div>
+                        <div>{{ car.transmission_type }}</div>
                     </div>
                     <div class="flex space-x-3 items-center font-medium">
                         <Gauge class="text-brand-primary"  size="16"/>
-                        <div>Mileage: 16,000 Km</div>
+                        <div>Mileage: {{car.mileage}} Km</div>
                     </div>
                     <div class="flex space-x-3 items-center font-medium">
                         <Cog class="text-brand-primary"  size="16"/>
-                        <div>Engine: 1.6 L</div>
+                        <div>Engine: {{car.engine_type.name}}</div>
+                    </div>
+                    <div class="flex space-x-3 items-center font-medium capitalize">
+                        <Fuel class="text-brand-primary" size="16"/>
+                        <div>Fuel Type: {{ car.fuel_type.name }}</div>
+                    </div>
+
+                    <div class="flex space-x-3 items-center font-medium capitalize">
+                        <Car class="text-brand-primary" size="16"/>
+                        <div>Body Type: {{ car.car_body_type.name }}</div>
                     </div>
                 </div>
                 <div class="hidden md:flex min-h-full w-0.5 bg-gray-300"></div>
                 <div class="flex flex-col space-y-6 md:w-5/12 md:justify-evenly">
                     <div class="flex space-x-3 items-center font-medium">
-                        <div class="w-3 h-3 rounded-full bg-gray-800"></div>
-                        <div>Exterior Color: Gray</div>
+                        <div class="w-3 h-3 rounded-full border border-gray-400" :style="'background-color:' + car.exterior_color.hex"></div>
+                        <div>Exterior Color: {{ car.exterior_color.name }}</div>
                     </div>
                     <div class="flex space-x-3 items-center font-medium">
-                        <div class="w-3 h-3 rounded-full bg-gray-300"></div>
-                        <div>Interior Color: Light Gray</div>
-                    </div>
-                    <div class="flex space-x-3 items-center font-medium">
-                        <Fuel class="text-brand-primary" size="16"/>
-                        <div>Fuel Type: Diesel</div>
+                        <div class="w-3 h-3 rounded-full border border-gray-400" :style="'background-color:' + car.interior_color.hex"></div>
+                        <div>Interior Color: {{ car.interior_color.name }}</div>
                     </div>
 
-                    <div class="flex space-x-3 items-center font-medium">
-                        <Car class="text-brand-primary" size="16"/>
-                        <div>Body Type: SUV</div>
+                    <div class="flex space-x-3 items-center font-medium capitalize">
+                        <RectangleHorizontal class="text-brand-primary" size="16"/>
+                        <div>Plate Type: {{ car.plate_type }}</div>
+                    </div>
+
+                    <div class="flex space-x-3 items-center font-medium capitalize">
+                        <MapPin class="text-brand-primary" size="16"/>
+                        <div>City: {{ car.city.name }}, {{car.location}}</div>
+                    </div>
+
+                    <div class="flex space-x-3 items-center font-medium capitalize">
+                        <ShieldBan class="text-brand-primary" size="16"/>
+                        <div>Accident: {{ car.accident_severity }}</div>
                     </div>
 
                 </div>
@@ -81,7 +102,7 @@ import ShareItem from "@/Views/Car/Show/ShareItem.vue";
             <ShareItem class="md:hidden"/>
         </div>
         <div class="hidden md:flex md:flex-col items-center justify-evenly w-5/12">
-            <SellerInfo/>
+            <SellerInfo :car="car"/>
             <ShareItem/>
         </div>
         </div>
