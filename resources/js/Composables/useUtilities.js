0e7767068748baf1793257723ef9b090b2d85354
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-05 13:29:11 +0300

    Add remove underscore from string helper function

--- resources/js/Composables/useUtilities.js
+++ resources/js/Composables/useUtilities.js
@@ -4,7 +4,11 @@ export function useUtilities(){
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
+    function removeUnderscores(string) {
+        return string.replace(/_/g, ' ');
+    }
     return {
-        formatNumberWithCommas
+        formatNumberWithCommas,
+        removeUnderscores
     }
 }
