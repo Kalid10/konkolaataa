ff830559d2f04588f97330d69e288a96ac9b1a6e
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-05 15:06:44 +0300

    Add to automatically enable the total switch if all items are selected

--- resources/js/Views/Filters/CheckBoxFilter.vue
+++ resources/js/Views/Filters/CheckBoxFilter.vue
@@ -32,14 +32,16 @@ const utilities = useUtilities();
 const allChecked = ref(false);
 const checkedItems = ref([]);
 
-onMounted(() => {
 // Initialize checkedItems with persisted data
+onMounted(() => {
     checkedItems.value = props.items.map(item => {
         const isChecked = props.initialCheckedItems.some(checkedItem => {
             return String(checkedItem.name) === String(item.name);
         });
         return { ...item, checked: isChecked };
     });
+    // Set allChecked if all items are initially checked
+    allChecked.value = checkedItems.value.every(item => item.checked);
 });
 
 const toggleAll = (checked) => {
@@ -50,17 +52,15 @@ const toggleAll = (checked) => {
 
 const toggleItem = (item) => {
     item.checked = !item.checked;
+    // Update allChecked state based on checkedItems
+    allChecked.value = checkedItems.value.every(item => item.checked);
     emitCheckedItems();
 };
 
 const emitCheckedItems = () => {
-    const selectedItems = checkedItems.value.filter(item => item.checked).map(item => ({ id: item.id, name: item.name }));
+    const selectedItems = checkedItems.value.filter(item => item.checked).map(item => ({id: item.id, name: item.name}));
     emit('update:checked', selectedItems, utilities.toCamelCase(props.title));
 };
-
-watch(allChecked, (newVal) => {
-    toggleAll(newVal);
-});
 </script>
 
 <template>
@@ -72,13 +72,14 @@ watch(allChecked, (newVal) => {
         <div class="flex flex-col space-y-3">
             <div class="flex items-center justify-between p-2 rounded-lg bg-slate-100">
                 <div>All {{ title }}'s</div>
-                <Switch :checked="allChecked" @update:checked="toggleAll"/>
+                <Switch v-model:checked="allChecked" @update:checked="toggleAll"/>
             </div>
             <div v-for="item in checkedItems" :key="item.id" class="flex items-center space-x-2 w-full pl-1">
                 <div class="flex space-x-2 items-center w-9/12">
-                    <Checkbox class="w-3.5 h-3.5" :checked="item.checked" @update:checked="() => toggleItem(item)" />
+                    <Checkbox class="w-3.5 h-3.5" :checked="item.checked" @update:checked="() => toggleItem(item)"/>
                     <div class="flex items-center space-x-1">
-                        <div v-if="showColorBadge" class="min-w-4 h-4 rounded-full border border-gray-300" :style="{ backgroundColor: item.hex }"></div>
+                        <div v-if="showColorBadge" class="min-w-4 h-4 rounded-full border border-gray-300"
+                             :style="{ backgroundColor: item.hex }"></div>
                         <span class="capitalize">{{ utilities.removeUnderscores(item.name) }}</span>
                     </div>
                 </div>
@@ -87,3 +88,7 @@ watch(allChecked, (newVal) => {
         </div>
     </div>
 </template>
+
+<style scoped>
+/* Your styles here */
+</style>
