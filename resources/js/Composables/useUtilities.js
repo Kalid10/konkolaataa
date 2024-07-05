7de5b62cfede253316c958bcd94b74677edef6e0
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-05 14:20:40 +0300

    Add camel-case and snake-case helper functions

--- resources/js/Composables/useUtilities.js
+++ resources/js/Composables/useUtilities.js
@@ -1,14 +1,26 @@
 
 export function useUtilities(){
-     function formatNumberWithCommas(number) {
+    function formatNumberWithCommas(number) {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
     function removeUnderscores(string) {
         return string.replace(/_/g, ' ');
     }
+    function toSnakeCase(string) {
+        return string.toLowerCase().replace(/\s+/g, "_");
+    }
+
+    function toCamelCase(string) {
+        return string.toLowerCase().replace(/_/g, ' ').replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+            return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
+        }).replace(/\s+/g, '');
+    }
+
     return {
         formatNumberWithCommas,
-        removeUnderscores
+        removeUnderscores,
+        toSnakeCase,
+        toCamelCase
     }
 }
