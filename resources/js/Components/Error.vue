4d316849182b7fae60c03543e2e6091a52dcf599
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-07 03:49:17 +0300

    Add error component

---
+++ resources/js/Components/Error.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+        v-if="error"
+        class="flex flex-col h-fit w-full items-center justify-center space-y-2 rounded-md  p-2 py-3 text-center text-xs md:text-xl font-medium "
+    >
+        <component v-if="icon" :is="icon" class="w-4 md:w-20" />
+        <CircleX v-else class="w-4 md:w-20" />
+        <span> {{ error }} ! </span>
+    </div>
+</template>
+<script setup>
+import {CircleX} from "lucide-vue-next";
+
+defineProps({
+    error: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        default: null,
+    },
+});
+</script>
+
+<style scoped></style>
--- resources/js/Pages/Car/Index.vue
+++ resources/js/Pages/Car/Index.vue
@@ -11,6 +11,7 @@ import {router, usePage} from "@inertiajs/vue3";
 import Pagination from "@/Components/Pagination.vue";
 import {LoaderCircle} from "lucide-vue-next";
 import Loading from "@/Components/Loading.vue";
+import Error from "@/Components/Error.vue";
 
 const cars = computed(()=>usePage().props.cars);
 
@@ -23,7 +24,7 @@ const routeToItem = (id) => {
     <div class="flex flex-col space-y-6 md:space-y-3">
         <Header title="Cars" subtitle="List of Cars" />
         <FilterDrawer/>
-        <div class="w-full flex  justify-center md:space-x-12">
+        <div class="w-full flex justify-center md:space-x-12">
             <div class="w-3/12 h-fit hidden md:flex flex-col items-center px-5 py-3 border border-gray-300 rounded-lg">
                 <DesktopFilter>
                     <template #used_tab>
@@ -34,10 +35,11 @@ const routeToItem = (id) => {
                     </template>
                 </DesktopFilter>
             </div>
-            <div class="w-full h-fit items-center md:items-start md:w-9/12 flex flex-col md:flex-row space-y-4 md:space-y-0 md:flex-wrap md:justify-between">
+            <div v-if="cars.data.length" class="w-full h-fit items-center md:items-start md:w-9/12 flex flex-col md:flex-row space-y-4 md:space-y-0 md:flex-wrap md:justify-between">
                 <Items @click="routeToItem(car.id)" v-for="car in cars.data" :car="car" :seller-type="i % 2 === 0 ? 'Private Seller':'Broker'" />
                 <Pagination :links="cars.links"/>
             </div>
+            <Error v-else error="No cars found"/>
         </div>
     </div>
 </template>
