4e6a3ac5ddd100f715ce927419835401ed875d41
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-08 20:06:47 +0300

    Add quick search for mobile devices

--- resources/js/Pages/Car/Index.vue
+++ resources/js/Pages/Car/Index.vue
@@ -10,6 +10,8 @@ import Pagination from "@/Components/Pagination.vue";
 import Error from "@/Components/Error.vue";
 import Loading from "@/Components/Loading.vue";
 import {XCircle} from "lucide-vue-next";
+import {Input} from "@/Components/shadcn/ui/input/index.js";
+import {debounce} from "lodash";
 
 const cars = computed(()=>usePage().props.cars);
 
@@ -21,11 +23,31 @@ const isLoading = ref(false);
 function checkLoading(value){
     isLoading.value = value
 }
+
+const searchKey = ref(usePage().props.search);
+const search = debounce(() => {
+    isLoading.value = true;
+    router.get(
+        "/car",
+        {
+            search: searchKey.value,
+        },
+        {
+            preserveState: true,
+            replace: true,
+            onFinish: () => {
+                isLoading.value = false;
+            }
+        }
+    );
+}, 900);
 </script>
 
 <template>
     <div class="flex flex-col space-y-6 md:space-y-3">
-        <Header title="Cars" subtitle="List of Cars" />
+        <Input class="sm:hidden rounded-full h-fit py-2  border-gray-400" placeholder="Quick Search" v-model="searchKey" @keyup="search"/>
+        <Header class="hidden sm:inline-block" title="Cars" subtitle="List of Cars" />
+
         <FilterDrawer/>
         <div class="w-full flex justify-center md:space-x-12">
             <div class="w-4/12 h-fit hidden md:flex flex-col items-center  border border-gray-300 rounded-lg">
@@ -35,7 +57,7 @@ function checkLoading(value){
                 </div>
                 <DesktopFilters @loading="checkLoading"/>
             </div>
-            <div class="relative w-full  justify-center items-center md:items-start md:w-9/12">
+            <div class="relative w-full  justify-center items-center md:items-start md:w-10/12">
                 <Loading v-if="isLoading" class="mt-40 md:mt-72"  />
 
                 <div v-else-if="cars.data.length && !isLoading" class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:flex-wrap md:justify-between">
