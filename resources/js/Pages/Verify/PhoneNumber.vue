6c6ec1839c844487ada4d33fb66915412245ec26
biniya <lemmasbini@gmail.com>
2024-07-08 13:50:54 +0300

    Add `pin-input` shad-cn component and update `PhoneNumber` verification page with it

--- resources/js/Pages/Verify/PhoneNumber.vue
+++ resources/js/Pages/Verify/PhoneNumber.vue
@@ -1,7 +1,12 @@
 <script setup>
+import { ref } from 'vue';
 import { useForm } from '@inertiajs/vue3';
+import {
+    PinInput,
+    PinInputGroup,
+    PinInputInput,
+} from '@/Components/shadcn/ui/pin-input/index.js';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import InputError from '@/Components/InputError.vue';
 
@@ -9,7 +14,9 @@ const form = useForm({
     otp: '',
 });
 
-const submit = () => {
+const value = ref([]);
+const handleComplete = (e) => {
+    form.otp = e.join('');
     form.post(route('verify.phone.submit'), {
         onFinish: () => form.reset('otp'),
     });
@@ -17,27 +24,37 @@ const submit = () => {
 </script>
 
 <template>
-    <div class="min-h-screen flex items-center justify-center">
-        <form @submit.prevent="submit" class="w-full max-w-md">
+    <div class="min-h-screen flex flex-col items-center justify-center gap-4">
+        <p class="mt-4 text-center text-gray-600 md:w-4/12 rounded-2xl shadow py-2 px-4">
+            You will receive an SMS with a one-time password (OTP). Please enter the 6-digit OTP in the fields below to verify your phone number.
+        </p>
+        <form @submit.prevent="handleComplete(value)" class="w-full max-w-md">
             <div>
-                <InputLabel for="otp" value="OTP" />
-                <TextInput
-                    id="otp"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.otp"
-                    required
-                    autofocus
-                    autocomplete="one-time-code"
-                    placeholder="Enter OTP"
-                />
-                <InputError :message="form.errors.otp" class="mt-2" />
+                <InputLabel for="otp" value="OTP" class="flex justify-center"/>
+                <div class="mt-1 flex w-full justify-center">
+                    <PinInput
+                        id="pin-input"
+                        v-model="value"
+                        placeholder="○"
+                        @complete="handleComplete"
+                    >
+                        <PinInputGroup>
+                            <PinInputInput
+                                v-for="(id, index) in 6"
+                                :key="id"
+                                :index="index"
+                            />
+                        </PinInputGroup>
+                    </PinInput>
+                </div>
+                <InputError :message="form.errors.otp" class="mt-2 flex justify-center"/>
             </div>
-            <div class="mt-6">
-                <PrimaryButton class="w-full" :disabled="form.processing">
+            <div class="mt-6 flex justify-center">
+                <PrimaryButton class="" :disabled="form.processing">
                     Verify
                 </PrimaryButton>
             </div>
         </form>
     </div>
 </template>
+
