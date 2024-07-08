1f6e5a393c81a2c2a3f99d3b3c69b3e6fcb1c666
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-08 18:09:30 +0300

    Add max width

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex justify-center items-center px-4 py-2 bg-brand-primary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-brand-primary focus:bg-brand active:bg-brand-primary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        class="inline-flex justify-center items-center px-4 py-2 bg-brand-primary border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-brand-primary focus:bg-brand active:bg-brand-primary focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 max-w-sm"
     >
         <slot />
     </button>
