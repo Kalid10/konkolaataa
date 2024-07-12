d316c29ba9d194cea78fe70380c80a335d1d345b
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-12 14:22:08 +0300

    Add tags component

---
+++ resources/js/Components/Tags.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    tag: {
+        type: String,
+        required: false
+    }
+})
+</script>
+
+<template>
+    <div
+        class="flex w-fit space-x-1 bg-gray-100 font-semibold border border-black px-1.5 py-0.5 mr-3 mt-1.5 capitalize rounded-md items-center text-xs">
+        <div v-if="tag">
+            {{ tag }}
+        </div>
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
