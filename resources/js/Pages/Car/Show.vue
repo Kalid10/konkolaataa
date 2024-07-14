b2670e895ee1c5075f745a4a96454ff632d78051
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-14 19:04:04 +0300

    Add open image carousel on show car page

--- resources/js/Pages/Car/Show.vue
+++ resources/js/Pages/Car/Show.vue
@@ -3,10 +3,10 @@ import { Antenna, Fuel, Cog, Gauge, Car, RectangleHorizontal, MapPin, ShieldBan}
 import SellerInfo from "@/Views/Car/Show/SellerInfo.vue";
 import ShareItem from "@/Views/Car/Show/ShareItem.vue";
 import {useUtilities} from "@/Composables/useUtilities.js";
-import {Input} from "@/Components/shadcn/ui/input/index.js";
-import InputLabel from "@/Components/InputLabel.vue";
+import {computed, ref} from "vue";
+import FullPageCarousel from "@/Components/FullPageCarousel.vue";
 
-defineProps({
+const props = defineProps({
     car: {
         type: Object,
         required: true
@@ -14,15 +14,29 @@ defineProps({
 });
 
 const utilities = useUtilities()
+const openCarousel = ref(false);
+const currentSlide = ref(1)
+
+const images = computed(() => props.car.images.map(image => image.url));
+
+function openImageCarousel(value) {
+    openCarousel.value = true;
+    currentSlide.value = value;
+}
 </script>
 
 <template>
     <div class="flex flex-col space-y-4 md:space-y-10 max-w-7xl mx-auto">
+        <FullPageCarousel
+            v-model:on="openCarousel"
+            :images="images"
+            :current-slide="currentSlide"
+        />
         <div class="flex justify-between space-x-10 ">
             <div class="w-full md:w-6/12 flex flex-col space-y-4 justify-evenly">
-                <img src="../../../../public/assets/images/pl.jpg" class="rounded-lg h-56 md:h-[22rem] w-full object-cover">
+                <img @click="openImageCarousel(2)" src="../../../../public/assets/images/pl.jpg" class="rounded-lg h-56 md:h-[22rem] w-full object-cover" alt="Image One">
                 <div class="flex items-center space-x-5 min-w-full overflow-x-auto hide-scrollbar">
-                    <img v-for="i in 5" src="../../../../public/assets/images/pl.jpg" :class="i === 1 ? '':'opacity-50'" class=" cursor-pointer rounded-lg h-20 min-w-28 object-cover">
+                    <img  v-for="i in 5" src="../../../../public/assets/images/pl.jpg" :class="i === 1 ? '':'opacity-50'" class=" cursor-pointer rounded-lg h-20 min-w-28 object-cover" alt="">
                 </div>
             </div>
             <div class="flex w-6/12 min-h-full space-x-6 justify-between">
@@ -35,7 +49,7 @@ const utilities = useUtilities()
         </div>
 
         <div class="flex flex-col md:flex-row w-full justify-between">
-        <div class="flex flex-col space-y-4 md:space-y-6 w-full md:bg-gray-50 md:px-5 md:pb-10 pt-5 rounded-lg md:w-6/12">
+        <div class="flex flex-col space-y-4 md:space-y-6 w-full md:bg-gray-50 md:px-5 md:pb-10 pt-5 rounded-lg md:w-7/12">
             <div class="flex justify-between items-center">
                 <div class="text-3xl md:text-4xl font-semibold">{{car.car_model.car_brand.name}} {{car.car_model.name}} </div>
             </div>
@@ -62,22 +76,21 @@ const utilities = useUtilities()
                         <Gauge class="text-brand-primary"  size="16"/>
                         <div>Mileage: {{car.mileage}} Km</div>
                     </div>
-                    <div class="flex space-x-3 items-center font-medium">
-                        <Cog class="text-brand-primary"  size="16"/>
-                        <div>Engine: {{car.engine_type.name}}</div>
-                    </div>
                     <div class="flex space-x-3 items-center font-medium capitalize">
                         <Fuel class="text-brand-primary" size="16"/>
                         <div>Fuel Type: {{ car.fuel_type.name }}</div>
                     </div>
-
+                    <div v-if="car.fuel_type.id=3" class="flex space-x-3 items-center font-medium">
+                        <Cog class="text-brand-primary"  size="16"/>
+                        <div>Range: {{car?.electric_car_range}}Km</div>
+                    </div>
                     <div class="flex space-x-3 items-center font-medium capitalize">
                         <Car class="text-brand-primary" size="16"/>
                         <div>Body Type: {{ car.car_body_type.name }}</div>
                     </div>
                 </div>
                 <div class="hidden md:flex min-h-full w-0.5 bg-gray-300"></div>
-                <div class="flex flex-col space-y-6 md:w-5/12 md:justify-evenly">
+                <div class="flex flex-col space-y-6 md:w-6/12 md:pl-2 md:justify-evenly">
                     <div class="flex space-x-3 items-center font-medium">
                         <div class="w-3 h-3 rounded-full border border-gray-400" :style="'background-color:' + car.exterior_color.hex"></div>
                         <div>Exterior Color: {{ car.exterior_color.name }}</div>
@@ -85,6 +98,11 @@ const utilities = useUtilities()
                     <div class="flex space-x-3 items-center font-medium">
                         <div class="w-3 h-3 rounded-full border border-gray-400" :style="'background-color:' + car.interior_color.hex"></div>
                         <div>Interior Color: {{ car.interior_color.name }}</div>
+                    </div>
+
+                    <div class="flex space-x-3 items-center font-medium">
+                        <Cog class="text-brand-primary"  size="16"/>
+                        <div>Engine: {{car.engine_type.name}}</div>
                     </div>
 
                     <div class="flex space-x-3 items-center font-medium capitalize">
@@ -106,7 +124,7 @@ const utilities = useUtilities()
             </div>
             <ShareItem class="md:hidden"/>
         </div>
-        <div class="hidden md:flex md:flex-col items-center justify-evenly space-y-6 w-5/12">
+        <div class="hidden md:flex md:flex-col items-center justify-evenly space-y-6 w-4/12">
 
             <div class="w-full p-3 flex flex-col space-y-2 bg-gray-50 rounded-lg shadow-sm">
                 <div class="font-medium text-lg">Description</div>
