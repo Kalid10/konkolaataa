ce6baa9a716249d1a5421809fd0c1234b14df308
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-16 13:14:10 +0300

    Add seller phone_-number

--- resources/js/Views/Car/Show/SellerInfo.vue
+++ resources/js/Views/Car/Show/SellerInfo.vue
@@ -11,7 +11,6 @@ const props = defineProps({
     },
 })
 
-// Calculate the broker fee
 const brokerFee = computed(() => {
     if (props.car.seller_type === 'broker') {
         return props.car.price * props.car.percentage / 100;
@@ -25,9 +24,9 @@ const utilities = useUtilities();
 <template>
     <div class="flex flex-col w-full md:text-lg h-fit space-y-2 md:space-y-4 bg-brand-primary p-3 md:p-5 rounded-lg text-white">
         <div class="text-2xl md:text-4xl font-bold w-fit">{{car.user.name}}</div>
-        <div class="font-medium">0921618103</div>
+        <div class="font-medium">{{ car.phone_number }}</div>
         <div v-if="car.seller_type === 'broker'" class="text-sm md:text-base font-medium capitalize">{{ car.seller_type }} X {{car.percentage}}% = {{utilities.formatNumberWithCommas(brokerFee)}} Br</div>
-        <div v-else class="text-sm md:text-base font-medium"> {{car.seller_type}}</div>
+        <div v-else class="text-sm md:text-base font-medium capitalize"> {{car.seller_type}}</div>
         <div class="hidden md:flex  justify-between">
             <div class="font-medium">{{car.city.name}}, {{car.location}}</div>
             <div class="flex space-x-3 md:space-x-10">
