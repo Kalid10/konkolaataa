15cfb531dc351ad6a8477089fb400e50ffed288c
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-23 19:12:47 +0300

    Change layout max width

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen w-full flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
+    <div class="min-h-screen max-w-screen-2xl mx-auto w-full flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
 
         <div
             class="w-full h-full px-6 bg-white  sm:rounded-lg"
