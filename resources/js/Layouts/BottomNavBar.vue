972089ce2017f185c630892f4bda3af68675c269
Kalid Abdu <61817693+Kalid10@users.noreply.github.com>
2024-07-24 00:15:11 +0300

    Add name and profile image on mobile navigation

--- resources/js/Layouts/BottomNavBar.vue
+++ resources/js/Layouts/BottomNavBar.vue
@@ -9,6 +9,9 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 const isLoggedIn = computed(() => {
     return usePage().props.auth.user;
 });
+const user = computed(() => {
+    return usePage().props.auth.user;
+});
 </script>
 
 <template>
@@ -16,8 +19,18 @@ const isLoggedIn = computed(() => {
     <div class="min-h-screen h-full bg-white">
         <main class="px-3 md:px-6 w-11/12 mx-auto md:w-11/12 bg-white pb-16 md:py-6">
             <div class="flex items-center py-6 justify-between w-full">
-                <div class="font-bold text-2xl text-brand-primary text-center uppercase">Konkolata</div>
-                <CirclePlusIcon class="fill-black text-white"/>
+                <div class="font-bold text-xl text-brand-primary text-center uppercase">Konkolata</div>
+                <div class="flex justify-end w-full space-x-2">
+                    <div class="text-xs font-medium items-end justify-center flex flex-col">
+                        <span>{{user.name}}</span>
+                        <span class="capitalize">{{ user.type }}</span>
+                    </div>
+                    <img
+                        :src="'https://api.dicebear.com/9.x/fun-emoji/svg/seed=' + usePage().props.auth.user.name"
+                        alt="avatar"
+                        class="w-11 h-11 rounded-md"
+                    />
+                </div>
             </div>
             <Notification/>
             <slot/>
